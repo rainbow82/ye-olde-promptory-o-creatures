fdfb859fada94d1a2918048c5dc542062cd56cf3
rainbow82 <shannonbeach1982@gmail.com>
2016-09-22 20:38:05 -0400

    updated detail method to use action results

---
+++ src/ye-olde-promptory-o-creatures/Controllers/CreaturesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ye_olde_promptory_o_creatures.Controllers
+{
+    public class CreaturesController :Controller
+    {
+        public ActionResult Detail()
+        {
+            return new ContentResult()
+            {
+                Content = "Hello from the creatures"
+            };
+        }
+    }
+}
