4ab330c4e70fbb52e0973459e63db7eef567836a
rainbow82 <shannonbeach1982@gmail.com>
2016-09-24 20:12:02 -0400

    added creatures model

---
+++ src/ye-olde-promptory-o-creatures/Models/Creatures.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ye_olde_promptory_o_creatures.Models
+{
+    public class Creatures
+    {
+        public int Id { get; set; }
+        public string CreatureName { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+
+    }
+}
