96018dee47f28a1400d647f988c5b75ef924ace9
rainbow82 <shannonbeach1982@gmail.com>
2016-11-05 16:38:03 -0400

    Updated Creature Model

--- src/ye-olde-promptory-o-creatures/Models/Creatures.cs
+++ src/ye-olde-promptory-o-creatures/Models/Creatures.cs
@@ -8,10 +8,30 @@ namespace ye_olde_promptory_o_creatures.Models
     public class Creatures
     {
         public int Id { get; set; }
+        public string CreatureType { get; set; }
         public string CreatureName { get; set; }
         public string Location { get; set; }
         public string Description { get; set; }
 
+        public string DisplayText
+        {
+            get
+            {
+                return CreatureName;
+            }
+        }
 
-    }
+        public string ImageFileName
+        {
+            get
+            {
+                return CreatureType.Replace(" ", "-")
+                    .ToLower() + "-" + CreatureName + ".jpg";
+            }
+        }
+
+      
+    }//end class
+
+   
 }
