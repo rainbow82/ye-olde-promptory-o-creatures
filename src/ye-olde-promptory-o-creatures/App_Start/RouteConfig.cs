2630127cb73dc560f68f52034f6c29c75759a1be
rainbow82 <shannonbeach1982@gmail.com>
2016-11-06 11:50:00 -0500

    updated the default route to be the home page

--- src/ye-olde-promptory-o-creatures/App_Start/RouteConfig.cs
+++ src/ye-olde-promptory-o-creatures/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ye_olde_promptory_o_creatures
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Creatures", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
